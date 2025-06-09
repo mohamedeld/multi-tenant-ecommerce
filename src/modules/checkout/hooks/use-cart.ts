d38361978c834dd088c701447aca93530a54ba37
mohamedeld <mohamed.azoz20010@gmail.com>
2025-06-09 21:30:36 +0300

    add enhancment to use store

--- src/modules/checkout/hooks/use-cart.ts
+++ src/modules/checkout/hooks/use-cart.ts
@@ -1,16 +1,25 @@
+import { useCallback } from "react";
 import { useCartStore } from "../store/use-cart-store"
 
 export const useCart = (tenantSlug:string)=>{
-     const {getCartByTenant,addProduct,removeProduct,clearAllCarts,clearCart} = useCartStore();
+    //  const {getCartByTenant,addProduct,removeProduct,clearAllCarts,clearCart} = useCartStore();
+
+     const getCartByTenant = useCartStore((state)=> state.getCartByTenant);
+     const addProduct = useCartStore((state)=> state.addProduct);
+     const removeProduct = useCartStore((state)=> state.removeProduct);
+     const clearAllCarts = useCartStore((state)=> state.clearAllCarts);
+     const clearCart = useCartStore((state)=> state.clearCart);
+
      const productIds = getCartByTenant(tenantSlug);
 
-     const toggleProduct = (productId:string)=>{
+     const toggleProduct = useCallback((productId:string)=>{
         if(productIds?.includes(productId)){
             removeProduct(tenantSlug,productId)
         }else{
             addProduct(tenantSlug,productId)
         }
-     }
+     },[addProduct,removeProduct,productIds,tenantSlug]);
+
      const isProductInCart = (productId:string)=>{
         return productIds?.includes(productId);
      }
